b8096e44b5c07dc194596186235db06143a3fe1f
CahTarine <camille.tarine@gmail.com>
2025-05-22 17:28:11 -0300

    rotas e context

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from '@phosphor-icons/react'
+import { GithubLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from '@phosphor-icons/react'
 
 function Footer() {
 
@@ -13,9 +13,18 @@ function Footer() {
                         </p>
                     <p className='text-lg'>Acesse minhas redes sociais</p>
                     <div className='flex gap-2'>
-                        <LinkedinLogoIcon size={48} weight='bold' />
-                        <InstagramLogoIcon size={48} weight='bold' />
-                        <FacebookLogoIcon size={48} weight='bold' />
+                        <a href="https://www.linkedin.com/in/camille-tarine/" target='_blank'>
+                            <LinkedinLogoIcon size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/camille.tarine?igsh=MWhyYnJuNzFrd29lbQ==" target='_blank'>
+                            <InstagramLogoIcon size={48} weight='bold' />
+                        </a>
+                        
+                        <a href="https://github.com/CahTarine" target='_blank'>
+                            <GithubLogoIcon size={48} weight='bold' />
+                        </a>
+                        
+                        
                     </div>
                 </div>
             </div>
